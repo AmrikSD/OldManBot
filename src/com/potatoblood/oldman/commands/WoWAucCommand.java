1b7c60c76d4c7cfd6eca7cb96b51eb25aaf480a4
AmrikSD <Amrik.Singh@Live.Co.Uk>
2018-08-10 04:45:02 +0100

    Cleaned up a little

--- src/com/potatoblood/oldman/commands/WoWAucCommand.java
+++ src/com/potatoblood/oldman/commands/WoWAucCommand.java
@@ -10,34 +10,29 @@ public class WoWAucCommand extends Command {
 
 	@Override
 	public void onCommand(MessageReceivedEvent e, String[] args) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public List<String> getAliases() {
-		// TODO Auto-generated method stub
 		return Arrays.asList("!wauc");
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
 		return "Returns the auction house information on the specified item";
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "World of Warcraft Auction Command";
 	}
 
 	@Override
 	public List<String> getUsageInstructions() {
-		// TODO Auto-generated method stub
 		List<String> temps = new ArrayList<String>();
-		temps.add("!wauc - [item]");
-		temps.add("[item] - The item you wish to find information on in the auction house");
+		temps.add("!wauc - <item>");
+		temps.add("<item> - The item you wish to find information on in the auction house");
 		return temps;
 	}
 
